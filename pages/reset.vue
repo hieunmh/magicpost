52f117d4c453576639a5f4f2fcf9d14d8efb054e
hieunmh <hieunm.pt@gmail.com>
2023-12-04 12:43:46 +0700

    reset password

--- pages/reset.vue
+++ pages/reset.vue
@@ -18,38 +18,29 @@
         <h1 class="text-3xl text-center font-semibold hidden lg:block text-gray-500">Cập nhật mật khẩu</h1>
         <form class="mt-10" @submit.prevent="">
           <div class="mt-6 h-16">
-            <div class="flex border-b-[1px] border-b-gray-400 items-center" :class="phoneError ? 'border-b-red-500' : ''">
-              <Icon name="material-symbols:call-sharp" size="30" class="text-gray-400" />
+            <div class="flex border-b-[1px] border-b-gray-400 items-center" :class="emailError ? 'border-b-red-500' : ''">
+              <Icon name="material-symbols:person" size="30" class="text-gray-400" />
               <input type="text" class=" w-full focus:outline-none pl-2 font-semibold text-[18px] text-gray-500"
-                placeholder="Số điện thoại" v-model="phone" maxlength="10"
+                placeholder="Email" v-model="email"
                 @blur="() => {
-                  if (phone.length < 1) {
-                    phoneError = 'Vui lòng nhập số điện thoại của bạn'
+                  if (email.length < 1) {
+                    emailError = 'Vui lòng nhập email của bạn'
                   }
-                  if (phone.length == 10) {
-                    phoneError = '';
-                    phone = `(+84) ${phone.substring(1, 4)} ${phone.substring(4, 7)} ${phone.substring(7, 10)}`
-                  }
+                  
                 }"
                 @input="() => {
                   incorrectError = '';
                   
                 }"
-                @focus="() => {
-                  if (phone.length == 17) {
-                    phone = '0' + phone.substring(6, 9) + phone.substring(10, 13) + phone.substring(14, 17);
-                  }
-                }"
-                oninput="this.value = this.value.replace(/[^0-9.]/g, '')"
               >
             </div>
-            <p class="text-red-500 font-semibold mt-1 text-[14px]">{{ phoneError }}</p>
+            <p class="text-red-500 font-semibold mt-1 text-[14px]">{{ emailError }}</p>
           </div>
           
     
 
-          <button class="mt-6 w-full rounded-lg h-12 font-semibold"
-          :class="phone && phoneError.length < 1
+          <button @click="resetPassword()" class="mt-6 w-full rounded-lg h-12 font-semibold"
+          :class="email && emailError.length < 1
             ? 'bg-[#05445e] text-white': 'bg-[#e8e8e8] text-gray-500'"
           > 
             <Icon v-if="isLoading" name="eos-icons:loading" size="30"  />
@@ -73,24 +64,25 @@ const router = useRouter();
 
 definePageMeta({middleware: 'loggedin'});
 
-let phone = ref<string>('');
-let phoneError = ref<string>('');
+let email = ref<string>('');
+let emailError = ref<string>('');
 let incorrectError = ref<string>('');
 let isLoading = ref<boolean>(false);
 
+watch(() => email.value, () => {
+  if (email.value.length == 0) {
+    emailError.value = 'Vui lòng nhập email của bạn';
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
+    emailError.value = 'Email không hợp lệ';
+  } else {
+    emailError.value = '';
+  }
+}) 
 
-watch(() => phone.value, () => {
-  if (phone.value.startsWith('(+84)')) return;
-  if (!phone.value.startsWith('0') ) {
-    phoneError.value = 'Số điện thoại phải bắt đầu bằng 0';
-  }
-  else if (phone.value.length < 10 && phone.value.length > 0) {
-    phoneError.value = 'Số điện thoại không hợp lệ';
-  }
-  else {
-    phoneError.value = '';
-  }
-})
-
+const resetPassword = async () => {
+  await client.auth.resetPasswordForEmail(email.value, {
+    redirectTo: 'http://localhost:3000/reset',
+  })
+}
 
 </script>
