eff6aca08af15db9e00b8d9b0a54066e51c93885
hieunmh <hieunm.pt@gmail.com>
2023-12-05 23:24:30 +0700

    add supabase type

---
+++ server/api/auth/register.ts
@@ -0,0 +1,15 @@
+import type { Database } from "@/types/supabaseType";
+
+import { serverSupabaseClient } from '#supabase/server';
+import { serverSupabaseUser } from "#supabase/server";
+
+
+export default defineCachedEventHandler(async (event) => {
+  const client = await serverSupabaseClient(event);
+  const server = await serverSupabaseUser(event);
+
+  const { user } = await readBody(event);
+
+  const { data } = await client.from('users').insert(user);
+
+})
